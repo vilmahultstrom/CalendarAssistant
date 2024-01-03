837fbdfa8b9b3cfc644d17d6c0ca11c9ae6f26b2
Wiljam Wilmi <wiljam.wilmi@hotmail.com>
2024-01-03 09:26:43 +0100

    min sdk 26, target sdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.calendarassistant"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.calendarassistant"
-        minSdk = 33
-        targetSdk = 33
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
